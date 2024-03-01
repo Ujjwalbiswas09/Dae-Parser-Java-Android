8a76cffd32dbea4cdb7b9e73d4b7e2b6de276929
Ujjwal Biswas <107708488+Ujjwalbiswas09@users.noreply.github.com>
2024-03-01 08:56:10 -0800

    Update Animator.java

--- Engine/Animation/Animator.java
+++ Engine/Animation/Animator.java
@@ -22,18 +22,19 @@ import com.badlogic.gdx.math.*;public class Animator
 								float val = dis2 / dis1;
 
 								if(delta >= current){
-												index++;
-										}
+								index++;
+								}
+					
 								if(!(index+1 < anime.timeStamps.length)){
-												index =0;
-												delta=0;
-										}
+								index =0;
+								delta=0;
+								}
 								currentFrame.clear();
 								for(int i=0;i < anime.parts.size();i++){
-												AnimationPart part = anime.parts.get(i);
-												float[] matrix1 = part.join_transforma.get(index);
-												float[] matrix2 = part.join_transforma.get(index+1);
-												currentFrame.put(part.name,calculate(matrix1,1-val,matrix2,val));
+								AnimationPart part = anime.parts.get(i);
+								float[] matrix1 = part.join_transforma.get(index);
+								float[] matrix2 = part.join_transforma.get(index+1);
+								currentFrame.put(part.name,calculate(matrix1,val,matrix2,1-val));
 										}
 
 						}
@@ -41,8 +42,8 @@ import com.badlogic.gdx.math.*;public class Animator
 				public static float[] calculate(float[] a,float v1,float[] b,float v2){
 								float[] mat = new float[a.length];
 								for(int i=0;i<mat.length;i++){
-												mat[i] = (b[i] * v1) + (a[i] * v2);
-										}
+								mat[i] = (b[i] * v1) + (a[i] * v2);
+								}
 								return mat;
 						}
 		}
